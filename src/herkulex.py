22a7c7446fe932baf324bff2fa28f024d5ceaef5
achu <achu@achu-Aspire-5738.(none)>
2014-05-02 12:46:29 +0530

    Initaialized sources

---
+++ src/herkulex.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2.7
+
+"""
+This is a python library for interfacing the Herkulex range of smart servo motors
+manufactured by Dongbu Robotics.
+
+The library was created by Achu Wilson (mailto:achu@sastrarobotics.com) for the internal projects of Sastra Robotics
+
+This free software is distributed under the GNU General Public License.
+See http://www.gnu.org/licenses/gpl.html for details.
+
+For usage of this code for  commercial purposes contact Sastra Robotics India Pvt. Ltd. (mailto:contact@sastrarobotics.com)
+
+""" 
+
+
